cf6ad5328607db31e2861dc75e4c5195373d4921
HrsCode11 <104048325+HrsCode11@users.noreply.github.com>
2023-10-25 05:33:40 +0700

    Update laundry.cpp

--- laundry.cpp
+++ laundry.cpp
@@ -6,20 +6,26 @@
 
 
 using namespace std;
+
+// Deklarasi fungsi untuk mencetak garis.
 garis(){
     cout << "---------------------------------" << endl;	
 }
 
 int main() {
+	// Deklarasi variabel-variabel yang diperlukan.
     char nama[50], nomor_hp[15], alamat[30], switchKategori[45], kategori, jenisBarang[20], nama_barang;
     int harga, total_bayar, berat, total, tanggalKembali, bulanKembali, tahunKembali;
+    // Mendapatkan waktu saat ini.
     time_t now = time(0);
     tm* date = localtime(&now);
 
+    // Menampilkan header laundry.
     cout << "=================================" << endl;
     cout << "\tCia-Cio Laundry" << endl;
     cout << "=================================" << endl << endl;
-
+    
+    // Input data customer
     cout << "\tData Customer Laundry" << endl;
     garis();
     cout << "Nama Customer   : "; cin >> nama;
@@ -30,7 +36,8 @@ int main() {
     cin >> tanggalKembali >> bulanKembali >> tahunKembali;
     garis();
 
-    system("cls");    
+    system("cls"); // Membersihkan layar.
+	// Memilih jenis barang.    
     cout << "\tJenis Barang" << endl;
     garis();
     cout << "1. Pakaian" << endl;
@@ -38,6 +45,7 @@ int main() {
     cout << "3. Gorden" << endl;
     garis();
     
+    // Pernyataan switch-case jenis barang.
     cout << "Pilih No Jenis Barang [1-3]  : "; cin >> nama_barang;
     switch (nama_barang) {
         case '1':
@@ -55,6 +63,7 @@ int main() {
     }
 
     system("cls");
+    // Memilih kategori cucian.
     cout << "\tKategori Cucian" << endl;
     garis();
     cout << "1. Reguler (2 Hari)" << endl;
@@ -62,6 +71,7 @@ int main() {
     cout << "3. Kilat (3 Jam)" << endl;
     garis();
     
+    // Pernyataan switch-case kategori.
     cout << "Pilih No Kategori [1-3]  : "; cin >> kategori;
     switch (kategori) {
         case '1':
@@ -80,11 +90,13 @@ int main() {
             strcpy(switchKategori, "Nomor Kategori Yang Dimasukkan Salah");
             break;
     }
-
+    
+    // Input berat cucian.
     cout << "Berat Cucian (Kg)        : "; cin >> berat;
     cout << "-------------------------------------------" << endl;
     
     system("cls");
+    // Menampilkan detail pemesanan laundry.
     cout << "\tDetail Pemesanan Ciao-Cio Laundry" << endl;
     cout << "-------------------------------------------" << endl;
     cout << "Nama Customer     : " << nama << endl;
@@ -97,8 +109,9 @@ int main() {
     cout << "Berat Cucian      : Kg." << berat << endl;   
     cout << switchKategori << "    : Rp. " << harga << endl;
     garis();
+    
+    // Menghitung dan menampilkan total harga.
     total = harga * berat;
     cout << "Total Harga     : Rp. " << total << endl;
-    return 0;
+    return 0; // Mengindikasi bahwa tidak ada kesalahan dalam program.
 }
-
